0a2a0457f5f97c5d2f6a0afd717ebedb891ba385
Igor Izviekov <igorizviekov@gmail.com>
2020-03-02 12:41:18 +0200

    initial

---
+++ src/components/Form.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="form">
+    <h1>Form</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Form"
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
